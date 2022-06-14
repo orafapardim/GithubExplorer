eccc23a4fe503c503df7d2e3326b14b4d373639a
orafapardim <orafapardim@gmail.com>
2022-06-14 09:34:58 -0300

    :construction: creating header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import { React } from 'react-dom';
+
+import './styles.scss'
+
+export function Header() {
+  return (
+    <header>
+      <div className="header-container">
+        <h1>Github Explorer</h1>
+      </div>
+    </header>
+  )
+}
