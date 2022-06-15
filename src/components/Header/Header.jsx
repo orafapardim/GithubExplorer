f0c2616a8bd100eab6f2bc21722cba4b80982279
orafapardim <orafapardim@gmail.com>
2022-06-15 12:53:07 -0300

    :construction: adding user image

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,6 +5,7 @@ export function Header() {
     <header>
       <div className="header-container">
         <h1>Github Explorer</h1>
+        <img alt="Foto de perfil de Rafael Pardim | orafapardim" src="https://github.com/orafapardim.png"/>
       </div>
     </header>
   )
